35e8636f55f977f55314ed57eccf16f43ece8b92
C1manyd <1225249223@qq.com>
2023-12-15 01:37:16 +0800

    Add KV operations to SyncMember

---
+++ kv.go
@@ -0,0 +1,11 @@
+package syncmember
+
+func (s *SyncMember) setKV(kv *KeyValuePayload) {
+
+}
+
+func (s *SyncMember) deleteKV(kv *KeyValuePayload) {
+}
+
+func (s *SyncMember) updateKV(kv *KeyValuePayload) {
+}
