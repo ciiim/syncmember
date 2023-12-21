8eb776d4b4ecdb1af8346a64bb69663895c30721
C1manyd <1225249223@qq.com>
2023-12-21 21:02:41 +0800

    Update sync members configuration

--- test/main_.go
+++ test/main_.go
@@ -11,9 +11,10 @@ import (
 
 func main() {
 	si := signal.NewManager()
-	s1 := syncmember.NewSyncMember("node1", syncmember.DebugConfig().SetPort(9000).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
-	s2 := syncmember.NewSyncMember("node2", syncmember.DebugConfig().SetPort(9001).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
-	s3 := syncmember.NewSyncMember("node3", syncmember.DebugConfig().SetPort(9002).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
+	s1 := syncmember.NewSyncMember("node1", syncmember.DebugConfig().SetPort(9001).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
+	s2 := syncmember.NewSyncMember("node2", syncmember.DebugConfig().SetPort(9002).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
+	s3 := syncmember.NewSyncMember("node3", syncmember.DebugConfig().SetPort(9003).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
+	s4 := syncmember.NewSyncMember("node4", syncmember.DebugConfig().SetPort(9004).SetLogLevel(slog.LevelInfo).OpenLogDetail(false))
 	si.AddWatcher(os.Interrupt, "shutdown", func() {
 		s2.Shutdown()
 		s1.Shutdown()
@@ -22,6 +23,7 @@ func main() {
 	go s1.Run()
 	go s2.Run()
 	go s3.Run()
+	go s4.Run()
 	time.Sleep(time.Second * 1)
 	s1.Join("172.26.123.188:9001")
 	time.Sleep(time.Second * 1)
