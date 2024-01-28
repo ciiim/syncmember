6be96a945efe2b5e30c5a12bd444c5c59f173293
C1manyd <1225249223@qq.com>
2024-01-28 18:04:13 +0800

    Add NodeEventDelegate interface methods

--- delegate.go
+++ delegate.go
@@ -3,10 +3,14 @@ package syncmember
 type NodeEventType int8
 
 type NodeEventDelegate interface {
+
+	// 当新节点加入时被调用
 	NotifyJoin(n *Node)
 
+	// 当已存在的节点变为存活时被调用
 	NotifyAlive(n *Node)
 
+	// 当已存在的节点变为死亡时被调用
 	NotifyDead(n *Node)
 }
 
